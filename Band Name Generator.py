9bfd19c729d4a82e3d509ab1d0b2b2d86c1bc7c6
adamsoyemi <96991981+adamsoyemi@users.noreply.github.com>
2022-01-03 02:13:07 +0000

    Delete Band Name Generator.py

--- Band Name Generator.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the Band Name Generator, with your host Adam Soyemi")
-city = input("What city did you grow up in?\n ")
-pet = input("What is the name of your first pet?\n ")
-print("In that case, introducing the newest band on the block,", str(city + " " + pet))
